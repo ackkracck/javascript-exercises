6cc8766e385e15485c8e15ce2c443f0b0e796474
Arnold M2 Macbook Air <54977051+ackkracck@users.noreply.github.com>
2025-10-15 18:39:42 +1100

    completed 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,31 @@
-const removeFromArray = function() {
+// I'm so dumb
+
+const removeFromArray = function (...args) {
+    const array = args[0];
+    const values = args.slice(1, args.length);
+
+    if (values.length === 0) { return array; }
+
+    let output = [];
+    let included = false;
+
+    for (let i = 0; i < array.length; i++) {
+
+        let checkValue = array[i];
+        included = false;
+
+        for (let j = 0; j < values.length; j++) {
+            if (values[j] === checkValue) {
+                included = true;
+            }
+        }
+
+        if (!included) {
+            output.push(checkValue);
+        }
+    }
+
+    return output;
 };
 
 // Do not edit below this line
