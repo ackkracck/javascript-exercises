74b40db28dd9391b656bf25785b23c465863e64a
Arnold M2 Macbook Air <54977051+ackkracck@users.noreply.github.com>
2025-10-15 20:09:40 +1100

    completed 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let output = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    output += array[i];
+  }
+
+  return output;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+	let output = 1;
 
+  for (let i = 0; i < array.length; i++) {
+    output *= array[i];
+  }
+
+  return output;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+
+  let output = 1;
+
+  for (let i = n; i > 0 ; i--) {
+    output *= i;
+  }
+
+  return output;
 };
 
 // Do not edit below this line
