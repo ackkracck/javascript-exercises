1e59ba626db74858a1f5a08563b2a473c27c1d0e
Arnold M2 Macbook Air <54977051+ackkracck@users.noreply.github.com>
2025-10-15 19:00:11 +1100

    completed 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
 
+    if (start < 0 || end < 0) { return "ERROR"; } // Handles negative numbers
+    if (!Number.isInteger(start) || !Number.isInteger(end)) { return "ERROR"; } // Handles non-int inputs
+
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
