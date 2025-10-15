7033545411d2bb1e8a1a9e9620dfc7597cf47f72
Arnold M2 Macbook Air <54977051+ackkracck@users.noreply.github.com>
2025-10-15 19:33:39 +1100

    completed 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let temperature = (f-32)/(9/5);
+  if (Number.isInteger(temperature)) {
+    return Number(temperature);
+  } else {
+    return Number(temperature.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let temperature = (c*(9/5)) + 32;
+  if (Number.isInteger(temperature)) {
+    return Number(temperature);
+  } else {
+    return Number(temperature.toFixed(1));
+  }
 };
 
 // Do not edit below this line
