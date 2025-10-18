cb128aee138c00b35eb072373c1daf0d233bb497
Arnold Christian <54977051+ackkracck@users.noreply.github.com>
2025-10-19 00:14:16 +1100

    i think i solved 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let index = 0;
+    let max_age = 0;
+
+    for (let i = 0; i < people.length; i++){
+        
+        let person_age = 0;
+
+        if (people[i].yearOfDeath){
+            person_age = people[i].yearOfDeath - people[i].yearOfBirth;
+        } else {
+            person_age = (new Date().getFullYear()) - people[i].yearOfBirth;
+        }
+
+        if (person_age > max_age) {
+            max_age = person_age;
+            index = i;
+        }
+
+    }
+
+    return people[index];
 };
 
 // Do not edit below this line
