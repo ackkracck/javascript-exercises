34a5542f16f35c1bf476afc180b6103eee38b4ce
Arnold Christian <arnoldcchristian@gmail.com>
2025-10-18 23:05:51 +1100

    worked on exercise 13, need to install node on windows first sigh

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+
+    let array = Array.from(input.toLowerCase());
+
+    let filtered_array = array.filter(element => letters.includes(element));
+
+    let reversed_array = [];
+
+    for (let i = filtered_array.length - 1; i = 0; i--){
+        reversed_array.push(filtered_array[i]);
+    }
+
+    return reversed_array === filtered_array;
 
 };
 
